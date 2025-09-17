8e3e7bed0b3c753fdbdd606140f0d5239919a463
Raj <rl945347@gmail.com>
2025-09-17 17:05:56 +0530

    Initial commit

---
+++ Smart-travel-planner/src/test/java/SpringBoot/Smart_travel_planner/SmartTravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot.Smart_travel_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTravelPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
